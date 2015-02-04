0f0e2689087abd7d144f0dd6fa03b0fe3719f9ff
jhd <ianxp0@gmail.com>
2015-02-04 20:35:49 +0000

    Added button

--- src/main.py
+++ src/main.py
@@ -1,18 +1,30 @@
 import sys
-from PyQt4 import QtGui
+from PyQt4 import QtGui, QtCore
 
+class Example(QtGui.QWidget):
+    
+    def __init__(self):
+        super(Example, self).__init__()
+        
+        self.initUI()
+        
+    def initUI(self):               
+        
+        qbtn = QtGui.QPushButton('KKAAWWWWWWWWEEEEEEEEEEEEEEEEE', self)
+        qbtn.clicked.connect(QtCore.QCoreApplication.instance().quit)
+        qbtn.resize(qbtn.sizeHint())
+        qbtn.move(50, 50)       
+        
+        self.setGeometry(750, 500, 750, 500)
+        self.setWindowTitle('KAWAII Patient Records')    
+        self.show()
+        
 def main():
+    
     print "KAWAII Patient Records"
     app = QtGui.QApplication(sys.argv)
-
-    w = QtGui.QWidget()
-    w.resize(750, 500)
-    w.move(300, 300)
-    w.setWindowTitle('KAWAII Patient Records')
-    w.show()
-    
+    ex = Example()
     sys.exit(app.exec_())
-
 
 if __name__ == '__main__':
     main()
