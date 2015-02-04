76e2bde6f932d54600418b5b64600850db6309ce
jhd <jonathanhennessydoylety@gmail.com>
2015-02-04 17:04:36 +0000

    Added basic QT window

--- src/main.py
+++ src/main.py
@@ -1,1 +1,18 @@
-print "Hello, World!"
+import sys
+from PyQt4 import QtGui
+
+def main():
+    
+    app = QtGui.QApplication(sys.argv)
+
+    w = QtGui.QWidget()
+    w.resize(750, 500)
+    w.move(300, 300)
+    w.setWindowTitle('KAWAII Patient Records')
+    w.show()
+    
+    sys.exit(app.exec_())
+
+
+if __name__ == '__main__':
+    main()
