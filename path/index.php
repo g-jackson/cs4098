b99ac7ed613d7f371c1353a2b7bd06aebc2bfad9
Glenn Jackson - Dazzle <gjackson@live.ie>
2015-03-04 20:24:19 +0000

    added index that collates all patients

--- path/index.php
+++ path/index.php
@@ -22,55 +22,72 @@
     }
   </style>
 
+  <!--get all xml files -->
+  <?php
+  $pid;
+  foreach(glob('*.dat.xml') as $file) {
+      $pid = $pid . ($file ."  ");
+  }
+  ?>
   <!-- get pid for passing to js -->
   <div id="dom-target" style="display: none;">
     <?php 
-        $pid = $_GET[pid];
         echo htmlspecialchars($pid); 
     ?>
   </div>
-
   <script> 
     //get pid for js   
     var div = document.getElementById("dom-target");
     var pid = div.textContent;
-    var file = pid.concat(".dat.xml");
-    file = file.replace(/\s/g, '');
-    //document.write(file);
-    if (window.XMLHttpRequest)
-      {// code for IE7+, Firefox, Chrome, Opera, Safari
-      xmlhttp = new XMLHttpRequest();
-      }
-    else
-      {// code for IE6, IE5
-      xmlhttp = new ActiveXObject("Microsoft.XMLHTTP");
-      }
 
-    xmlhttp.open("GET", file ,false);
-    xmlhttp.send();
-    xmlDoc = xmlhttp.responseXML;
+    var files = pid.match(/\S+/g);
+    /*
+    for (i=0;i<files.length;i++){
+      document.write(files[i]+"<br>");
+    }
+    */
+    document.write("patients = " +files.length+ "<br>");
 
     document.write("<table cellpadding=\"10\"><tr><th>\
-      Process ID</th><th>\
-      Model</th><th>\
-      Status</th><th>\
-      Graph</th></tr>");
-    var x = xmlDoc.getElementsByTagName("process");
-    for (i = 0; i < x.length; i++)
-      {
-      document.write("<tr><td>");
-      document.write(x[i].getAttribute("pid"));
-      document.write("</td><td>");
-      document.write(x[i].getAttribute("model"));
-      document.write("</td><td>");
-      document.write(x[i].getAttribute("status"));
-      document.write("</td><td>");
-      var graphlink = "/graph.php?pid=" + pid + '&' + "procid=" + i;
-      graphlink = graphlink.replace(/\s/g, '');
-      document.write("<a href="+graphlink+">Pathway</a>");
-      document.write("</td></tr>");
+        Patient ID</th><th>\
+        Process ID</th><th>\
+        Model</th><th>\
+        Status</th><th>\
+        Graph</th></tr>");
+
+    for (i =0; i< files.length;i++){
+      file = files[i];
+      //document.write(file + "<br>");
+      
+      if (window.XMLHttpRequest){
+        xmlhttp = new XMLHttpRequest(); // code for IE7+, Firefox, Chrome, Opera, Safari
       }
-    document.write("</table>");
+      else{
+        xmlhttp = new ActiveXObject("Microsoft.XMLHTTP"); // code for IE6, IE5
+      }
+
+      xmlhttp.open("GET", file ,false);
+      xmlhttp.send();
+      xmlDoc = xmlhttp.responseXML;
+      
+      var x = xmlDoc.getElementsByTagName("process");
+      for (j = 0; j < x.length; j++){
+        document.write("<tr><td>");
+        document.write(i);
+        document.write("</td><td>");
+        document.write(x[j].getAttribute("pid"));
+        document.write("</td><td>");
+        document.write(x[j].getAttribute("model"));
+        document.write("</td><td>");
+        document.write(x[j].getAttribute("status"));
+        document.write("</td><td>");
+        var graphlink = "/graph.php?pid=" + pid + '&' + "procid=" + j;
+        graphlink = graphlink.replace(/\s/g, '');
+        document.write("<a href="+graphlink+">Pathway</a>");
+        document.write("</td></tr>");
+      }
+  }
+  document.write("</table>");
   </script>
   </body>
 </html>
