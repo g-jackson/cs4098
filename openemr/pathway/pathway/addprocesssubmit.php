f3f127a5e4135ce63710e092608e03251b41fefb
Glenn Jackson - Dazzle <gjackson@live.ie>
2015-03-16 16:08:43 +0000

    updating to standard openemr

--- openemr/pathway/pathway/addprocesssubmit.php
+++ openemr/pathway/pathway/addprocesssubmit.php
@@ -17,7 +17,7 @@ $file = @$_POST[pml];
 //for ($i = 0; $i < sizeof($arr); $i++)
 //	echo $arr[$i];
 
-exec('../peos/os/kernel/peos -l '.$pid.' -c pml/'.$file.".pml 2>&1", $results);
+exec('./peos -l '.$pid.' -c pml/'.$file.".pml 2>&1", $results);
 //exec('../peos/os/kernel/peos -i 2>&1', $results);
 //echo sizeof($results);
 for ($i = 0; $i < sizeof($results); $i++)
