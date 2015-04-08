977d46851ec6842b7a031167815d27ac08f4e40d
jhd <ianxp0@gmail.com>
2015-04-08 12:34:38 +0100

    Improved basic http test

--- test/httptest.py
+++ test/httptest.py
@@ -1,12 +1,15 @@
 import urllib2
 
-url = "http://localhost/notthere"
-req = urllib2.Request(url)
-try:
-    resp = urllib2.urlopen(req)
-except urllib2.HTTPError as e:
-    print e.code
-    print url
-    exit(0)
+urls = ["http://localhost/openemr"]
+
+for url in urls:
+    req = urllib2.Request(url)
+    try:
+        resp = urllib2.urlopen(req)
+        print url + ": Passed"
+    except urllib2.HTTPError as e:
+        print e.code
+        print url
+        exit(0)
 print "All good!"
 
